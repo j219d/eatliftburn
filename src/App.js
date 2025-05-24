5a8ee7f3d3bfa17cd7f3a4dffe5b65594ab7a2ad
j219d <yonatanmdeutsch@gmail.com>
2025-05-24 23:45:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,6 +62,14 @@ function App() {
     { name: "2 eggs + butter", cal: 175, prot: 12 },
     { name: "2 eggs, 1 egg white + butter", cal: 190, prot: 15 },
     { name: "Yogurt 0%", cal: 117, prot: 20 }
+    { name: "Cottage cheese", cal: 100, prot: 11 },
+  { name: "Quest Chips", cal: 140, prot: 20 },
+{ name: "Spinach (cooked portion)", cal: 15, prot: 1.5 },
+{ name: "Cucumber (1 medium)", cal: 16, prot: 1 },
+{ name: "Carrot (1 medium)", cal: 25, prot: 0.5 },
+{ name: "Pumpkin seeds (1 tbsp)", cal: 60, prot: 3 },
+{ name: "Flax seeds (1 tbsp)", cal: 55, prot: 2 },
+{ name: "Walnut (1 whole)", cal: 26, prot: 0.6 },
   ];
 
   const totalBurned = manualBurn;
