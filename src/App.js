577fd8a18e6288ee1996e8808f2fdda34be75f9e
j219d <yonatanmdeutsch@gmail.com>
2025-05-26 15:40:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -459,7 +459,7 @@ const navBtnStyle = {
     onChange={(e) => setCustomWorkout({ ...customWorkout, treadCal: e.target.value })}
     style={{
       width: "43px", // 🔻 halved
-      height: "30px",
+      height: "25px",
       padding: "6px",
       fontSize: "14px",
       borderRadius: "8px",
@@ -476,7 +476,7 @@ const navBtnStyle = {
     onChange={(e) => setCustomWorkout({ ...customWorkout, treadKm: e.target.value })}
     style={{
       width: "39.125px", // 🔻 halved
-      height: "30px",
+      height: "25px",
       padding: "6px",
       fontSize: "14px",
       borderRadius: "8px",
