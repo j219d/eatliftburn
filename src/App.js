83ce68086572ce93521a3dc0ec7be9cd4013fc42
j219d <yonatanmdeutsch@gmail.com>
2025-06-01 16:13:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend);
+
 
 const burnRates = {
   "Push-ups": 0.5,
